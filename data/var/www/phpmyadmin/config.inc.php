e18a3c99599a31fc619093b6ce57dc7575cd1486
Nathaniel van Diepen <eeems@omnimaga.org>
2014-09-24 18:17:29 -0600

    adding phpmyadmin

---
+++ data/var/www/phpmyadmin/config.inc.php
@@ -0,0 +1,46 @@
+<?php
+	/* Servers configuration */
+	$i = 0;
+
+	/* Local server */
+	$i++;
+	$cfg['Servers'][$i]['auth_type'] = 'cookie';
+	$cfg['Servers'][$i]['host'] = 'localhost';
+	$cfg['Servers'][$i]['connect_type'] = 'tcp';
+	$cfg['Servers'][$i]['compress'] = true;
+	$cfg['Servers'][$i]['verbose'] = gethostname();
+	$cfg['Servers'][$i]['port'] = '';
+	$cfg['Servers'][$i]['socket'] = '';
+	$cfg['Servers'][$i]['ssl'] = true;
+	$cfg['Servers'][$i]['user'] = 'root';
+	$cfg['Servers'][$i]['password'] = '';
+	$cfg['Servers'][$i]['pmadb'] = true;
+	$cfg['Servers'][$i]['relation'] = 'pma__relation';
+	$cfg['Servers'][$i]['table_info'] = 'pma__table_info';
+	$cfg['Servers'][$i]['table_coords'] = 'pma__table_coords';
+	$cfg['Servers'][$i]['pdf_pages'] = 'pma__pdf_pages';
+	$cfg['Servers'][$i]['column_info'] = 'pma__column_info';
+	$cfg['Servers'][$i]['bookmarktable'] = 'pma__bookmark';
+	$cfg['Servers'][$i]['history'] = 'pma__history';
+	$cfg['Servers'][$i]['designer_coords'] = 'pma__designer_coords';
+	$cfg['Servers'][$i]['recent'] = 'pma__recent';
+	$cfg['Servers'][$i]['table_uiprefs'] = 'pma__table_uiprefs';
+	$cfg['Servers'][$i]['tracking'] = 'pma__tracking';
+	$cfg['Servers'][$i]['userconfig'] = 'pma__userconfig';
+	$cfg['Servers'][$i]['navigationhiding'] = 'pma__navigationhiding';
+	$cfg['Servers'][$i]['savedsearches'] = 'pma__savedsearches';
+	$cfg['Servers'][$i]['users'] = 'pma__users';
+	$cfg['Servers'][$i]['controluser']   = 'pma';
+	$cfg['Servers'][$i]['controlpass']   = 'pmapass';
+
+	/* End of servers configuration */
+
+	$cfg['blowfish_secret'] = 'sEF45WT2LQEcxzUj$';
+	$cfg['UploadDir'] = '';
+	$cfg['SaveDir'] = '';
+	$cfg['ShowDbStructureCreation'] = true;
+	$cfg['ShowDbStructureLastUpdate'] = true;
+	$cfg['ShowDbStructureLastCheck'] = true;
+	$cfg['DefaultLang'] = 'en';
+	$cfg['ServerDefault'] = 1;
+?>
--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+# Config
+ENVIROMENT="prod";
+TMP="/tmp/omni-setup";
+if [[ "$1" != "" ]];then
+	ENVIROMENT="$1";
+fi;
+REGISTER_URL="http://api.omnimaga.org/register/$ENVIROMENT";
+PHPMYADMIN_URL="http://downloads.sourceforge.net/project/phpmyadmin/phpMyAdmin/4.2.9/phpMyAdmin-4.2.9-english.tar.xz";
 # Functions for logging
 section(){
 	echo "=> Starting Section \"$@\"";
@@ -29,6 +37,20 @@ if [ "$(id -u)" != "0" ]; then
 	exit;
 fi;
 # Actually run the install
+
+section "Registering";
+log "Getting IDs";
+mkdir -p /tmp/omni-setup;
+sublog "Hostname";
+wget -q "$REGISTER_URL/hostname" -O $TMP/hostname;
+if [[ "$(cat $TMP/hostname)" == "" ]];then
+	hostname > $TMP/hostname;
+fi;
+hostname $(cat $TMP/hostname);
+cp $TMP/hostname /etc/hostname;
+sublog "MySQL ID";
+wget -q "$REGISTER_URL/mysql-id" -O $TMP/mysql-id;
+
 section "Package Installation";
 log "LAMP Stack";
 install lamp-server^;
@@ -51,10 +73,18 @@ echo "export PATH=$PATH:/opt/omnimaga/bin;" > /etc/profile.d/omnimaga-server-uti
 . /etc/profile.d/omnimaga-server-utils.sh;
 sublog "getting files";
 rm -rf /opt/omnimaga/bin;
-git clone https://github.com/Omnimaga/server-utils.git /opt/omnimaga/bin/;
+git clone -q https://github.com/Omnimaga/server-utils.git /opt/omnimaga/bin/;
 chmod a+x /opt/omnimaga/bin/*;
 subinfo "Add users to the group omnimaga-utils to allow access";
-
+log "phpmyadmin";
+sublog "Downloading";
+wget -q $PHPMYADMIN_URL -O $TMP/pma.tar.xz;
+sublog "Extracting";
+tar -C $TMP/ -xf $TMP/pma.tar.xz;
+sublog "Copying";
+mkdir -p /var/www/phpmyadmin/;
+cp -R $TMP/phpMyAdmin-*/{*,.[a-zA-Z0-9]*} /var/www/phpmyadmin/;
+cp data/var/www/phpmyadmin/config.inc.php /var/www/phpmyadmin/;
 
 section "Config";
 log "Setting up sudoers";
@@ -66,3 +96,5 @@ updatesudo sudoers.d/web;
 sublog "ircd";
 groupadd -f ircd;
 updatesudo sudoers.d/ircd;
+sublog "mysqld";
+cp data/etc/mysql/conf.d/replication.cnf /etc/mysql/conf.d/replication.cnf;
